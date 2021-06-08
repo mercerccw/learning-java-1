8c780e1d03aa3b41e5cf06ce24baa2c0f67f8062
clayton.mercer <clayton.mercer@lifeomic.com>
2021-06-08 08:07:58 -0400

    Initial commit

---
+++ src/main/java/org/mercer_systems/App.java
@@ -0,0 +1,13 @@
+package org.mercer_systems;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/mercer_systems/AppTest.java
@@ -0,0 +1,20 @@
+package org.mercer_systems;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
