0b49b858db51c22f85d53978ee4d2da62bc8be70
clayton.mercer <clayton.mercer@lifeomic.com>
2021-06-08 09:37:28 -0400

    Add tests

--- src/main/java/org/mercer_systems/App.java
+++ src/main/java/org/mercer_systems/App.java
@@ -1,13 +1,37 @@
 package org.mercer_systems;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    /**
+     * Returns true if the number is even, and false if the number is odd
+     *
+     * @param number the number that will be tested
+     * @return boolean   the returned boolean
+     */
+    public static boolean isEven(int number) {
+        return false;
+    }
+
+
+    /**
+     * Returns "Foo" if the number is divisible by 3, returns "Bar" if the number is divisible by 5, and returns
+     * "FooBar" if the number is divisible by 3 and 5. If the number is neither, then return "Whoops"
+     *
+     * @param number the number that will be tested
+     * @return String   the returned String
+     */
+    public static String fooBar(int number) {
+        return "";
+    }
+
+    /**
+     * Returns "Foo" if the number is divisible by 3, returns "Bar" if the number is divisible by 5, and returns
+     * "FooBar" if the number is divisible by 3 and 5
+     *
+     * @param number    the index of the Fibonacci sequence
+     * @return int   the number at the index of number in the Fibonacci sequence
+     */
+    public static int fibonacci(int number) {
+        return 0;
     }
 }
--- src/test/java/org/mercer_systems/AppTest.java
+++ src/test/java/org/mercer_systems/AppTest.java
@@ -1,20 +1,32 @@
 package org.mercer_systems;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testIsEven() {
+        assertTrue(App.isEven(0));
+        assertTrue(App.isEven(2));
+        assertFalse(App.isEven(5));
+        assertFalse(App.isEven(101));
+    }
+    @Test
+    public void testFooBar() {
+        Assert.assertEquals("Foo",App.fooBar(3));
+        Assert.assertEquals("Bar", App.fooBar(5));
+        Assert.assertEquals("FooBar", App.fooBar(15));
+        Assert.assertEquals("Whoops", App.fooBar(4));
+    }
+    @Test
+    public void testFibonacci() {
+        Assert.assertEquals(2, App.fibonacci(3));
+        Assert.assertEquals(13, App.fibonacci(7));
+        Assert.assertEquals(75025, App.fibonacci(25));
+        Assert.assertEquals(832040, App.fibonacci(30));
     }
 }
